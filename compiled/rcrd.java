c6d21f710a29e0ab04de1d587c72fff88e4ef25a
Stephen Cormier <sjocormier@gmail.com>
2012-03-26 23:29:17 -0400

    Rewritten using NyARMultiBoard

--- compiled/rcrd.java
+++ compiled/rcrd.java
@@ -1,8 +1,6 @@
 import processing.core.*; 
 import processing.xml.*; 
 
-import java.io.*; 
-import processing.opengl.*; 
 import jp.nyatla.nyar4psg.*; 
 import codeanticode.gsvideo.*; 
 import fullscreen.*; 
@@ -33,45 +31,111 @@ public class rcrd extends PApplet {
 // 88          `"Ybbd8"' 88          `"8bbdP"Y8
 //
 // Stephen Cormier
-// 03-12-12
+// 03-26-12
 // 
 // 
 
 
 
 
-
-
-
-String camPara = "/Users/Steve/Documents/Processing/libraries/nyar4psg/data/camera_para.dat";
-String patternPath = "/Users/Steve/Documents/Processing/libraries/nyar4psg/patternMaker/examples/ARToolKit_Patterns";
+String camPara = "camera_para.dat";
+String patternPath = "patterns/";
+String songpPath = "songs/";
+String coverPath = "covers/";
 
 int arWidth = 640;
-int arHeight = 360;
+int arHeight = 480;
 
 int numMarkers = 6;
+double markerWidth = 135;
 
 GSCapture cam;
-MultiMarker nya;
+NyARMultiBoard nya;
 FullScreen fs;
+PFont font, font2d;
 
 public void setup(){
-	size(1680, 1050, OPENGL);
-	cam = new GSCapture(this, 640, 360);
+	size(640, 480, P3D);
+	colorMode(RGB, 100);
+	font = createFont("FFScala", 32);
+	font2d = createFont("FFScala", 10);
+	
+	cam = new GSCapture(this, arWidth, arHeight);
 	cam.start();
-	noStroke();
-	fs = new FullScreen(this);
-	fs.enter();
+	
+	String[] patterns = {"patt.hiro", "02.patt", "03.patt", "04.patt", "05.patt", "06.patt"};
+	
+	/*
+	String patterns[] = new String[numMarkers]; 
+	
+	for(int i = 1; i < numMarkers - 1; i++){
+		patterns[i-1] = patternPath + "0" + i + ".patt";
+	}
+	*/
+	
+	double[] widths = new double[numMarkers];
+	Arrays.fill(widths, markerWidth); 
+	
+	nya = new NyARMultiBoard(this, arWidth, arHeight, camPara, patterns, widths);
+	
+	nya.gsThreshold = 120;
+	
+	nya.cfThreshold = 0.4f;
 }
+
+// draw marker corners and also position in text
+public void drawMarkerPos(int[][] pos2d)
+{
+  textFont(font,10.0f);
+  stroke(100,0,0);
+  fill(100,0,0);
+  
+  // draw ellipses at outside corners of marker
+  for(int i=0;i<4;i++){
+    ellipse(pos2d[i][0], pos2d[i][1],5,5);
+  }
+  
+  fill(0,0,0);
+  for(int i=0;i<4;i++){
+    text("("+pos2d[i][0]+","+pos2d[i][1]+")",pos2d[i][0],pos2d[i][1]);
+  }
+}
+
 
 public void draw(){
-	if(cam.available()){
-		cam.read();
-		background(0);
-		image(cam, 0, 0, width, height);
+	if(cam.available() !=true){
+		return;
 	}
-}
+	
+	cam.read();
+	
+	hint(DISABLE_DEPTH_TEST);
+	
+	image(cam, 0, 0, width, height);
+	
+	hint(ENABLE_DEPTH_TEST);
+	
+	if(nya.detect(cam)){
+		hint(DISABLE_DEPTH_TEST);
+		
+		for(int i = 0; i <  nya.markers.length; i++){
+			if(nya.markers[i].detected){
+				drawMarkerPos(nya.markers[i].pos2d);
+			}
+		}
 
+	}
+	
+	hint(ENABLE_DEPTH_TEST);
+	
+	hint(DISABLE_DEPTH_TEST);
+  	textFont(font2d,10.0f);
+  	textMode(SCREEN);
+  	fill(100,100,0);
+  	text("frame rate = " + frameRate, 10, 10);
+  	textMode(MODEL);
+  	hint(ENABLE_DEPTH_TEST);
+}
   static public void main(String args[]) {
     PApplet.main(new String[] { "--bgcolor=#FFFFFF", "rcrd" });
   }
