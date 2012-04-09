e5447129dfd14a9a44d26620ccb44bf698fb7f08
Stephen Cormier <sjocormier@gmail.com>
2012-04-09 14:36:55 -0400

    more tweaking

--- compiled/rcrd.java
+++ compiled/rcrd.java
@@ -32,7 +32,7 @@ public class rcrd extends PApplet {
 // 88          `"Ybbd8"' 88          `"8bbdP"Y8
 //
 // Stephen Cormier
-// 04-01-12
+// 04-09-12
 // 
 // 
 
@@ -49,6 +49,7 @@ float markerWidth = 135.0f;
 int numMarkers = 6;
 
 AudioPlayer songs[] = new AudioPlayer[numMarkers];
+AudioMetaData meta[] = new AudioMetaData[numMarkers];;
 PImage covers[] = new PImage[numMarkers];
 
 public void setup(){
@@ -72,81 +73,73 @@ public void setup(){
                            "InTheAeroplaneOverTheSea",
                            "BirdsOnIce"};
     
-	size(640, 480, P3D);
-	colorMode(RGB, 100);
-	font = createFont("FFScala", 32);
-	font2d = createFont("FFScala", 10);
+    size(640, 480, P3D);
+    colorMode(RGB, 100);
+    font = createFont("FFScala", 32);
+    font2d = createFont("FFScala", 10);
 	
 	
-	Arrays.fill(widths, (double)markerWidth);
+    Arrays.fill(widths, (double)markerWidth);
 	
-	cam = new GSCapture(this, arWidth, arHeight);
-	cam.start();
+    cam = new GSCapture(this, arWidth, arHeight);
+    cam.start();
 
-	minim = new Minim(this);
+    minim = new Minim(this);
 
-	for(int i = 0; i <  numMarkers ; i++){
-	    patterns[i] = patternPath + "0" + (i+1) + ".patt";
-	}
+    for(int i = 0; i <  numMarkers ; i++){
+        patterns[i] = patternPath + "0" + (i+1) + ".patt";
+
+        covers[i] = loadImage(coverPath + songTitles[i] + ".jpg");
 	
-	for(int j = 0; j <  numMarkers; j++){
-		covers[j] = loadImage(coverPath + songTitles[j] + ".jpg");
-	}
+        songs[i] = minim.loadFile(songPath + songTitles[i] + ".aif");
+
+        meta[i] = songs[i].getMetaData();
+    }
 	
-	for(int k = 0; k <  numMarkers; k++){
-		songs[k] = minim.loadFile(songPath + songTitles[k] + ".aif");
-	}
-		
-	nya = new NyARMultiBoard(this, arWidth, arHeight, camPara, patterns, widths);
+    nya = new NyARMultiBoard(this, arWidth, arHeight, camPara, patterns, widths);
 	
-	nya.gsThreshold = 70;
-	nya.cfThreshold = 0.7f;
-	nya.lostDelay = 7;
+    nya.gsThreshold = 70;
+    nya.cfThreshold = 0.7f;
+    nya.lostDelay = 7;
 	
-	//fs = new FullScreen(this);
-	//fs.enter();
+    //fs = new FullScreen(this);
+    //fs.enter();
 }
 
-public void draw(){
-    
-    float prevAngles[] = new float[numMarkers];
-    Arrays.fill(prevAngles, 0);
-    int angleVal;
-    int prevVal;
-    int diff;
-	if(cam.available() !=true){
-	    return;
-	}
-	
-	cam.read();
-	
-	hint(DISABLE_DEPTH_TEST);
-	
-	image(cam, 0, 0, width, height);
-	
-	hint(ENABLE_DEPTH_TEST);
-	
-	if(nya.detect(cam)){
 
+public void draw(){
+
+    if(cam.available() !=true){
+        return;
+    }
+	
+    cam.read();
+	
+    hint(DISABLE_DEPTH_TEST);
+	
+    image(cam, 0, 0, width, height);
+	
+    hint(ENABLE_DEPTH_TEST);
+	
+    if(nya.detect(cam)){
+        
         for (int i=0; i <  nya.markers.length; i++)
-	    {
+        {
             if (nya.markers[i].detected)
             {
                 nya.markers[i].beginTransform();
                 image(covers[i], -(markerWidth/2), -(markerWidth/2), markerWidth, markerWidth);
+                translate(0,0,10);
+                noFill();
+                strokeWeight(1.5f);
+                stroke(0xff66FF33);
+                ellipse(0, 0, 250, 250);
+                line(0, -(markerWidth/2), 0, 0, -125, 0);
                 nya.markers[i].endTransform();
+                
                 songs[i].play();
+                songs[i].skip(25 * (int)(nya.markers[i].angle.z * 10));
                 
-                angleVal = (int)(nya.markers[i].angle.z * 10);
-                prevVal = (int)(prevAngles[i] * 10);
-                
-                //if(prevAngles[i] != nya.markers[i].angle.z){
-                    //diff = angleVal - prevVal;
-                    songs[i].skip(25 * (int)(nya.markers[i].angle.z * 10));
-                //}
-                
-                prevAngles[i] = nya.markers[i].angle.z;
-            
             }else{
                 songs[i].pause();
             }
